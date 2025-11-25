c9f8b1a1b72560ade6c176c2150872bfe6fd518e
Mutsuha Asada <me@momee.mt>
2025-11-25 17:07:33 +0900

    aicommit: init at 0.6.5

---
+++ pkgs/aicommit/default.nix
@@ -0,0 +1,27 @@
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  pname = "aicommit";
+  version = "0.6.5";
+in
+  pkgs.buildGoModule {
+    inherit pname version;
+
+    src = pkgs.fetchFromGitHub {
+      owner = "coder";
+      repo = pname;
+      tag = "v${version}";
+      hash = "sha256-GMtl6GcpWhnK3zo8ur8XzF6gMjJHrPpvVeQ9zTsWpcg=";
+    };
+
+    vendorHash = "sha256-bZQ0XAbyIr44NZko6O7SkUJBa3rkwblBsSU2jnak/+0=";
+
+    meta = {
+      description = "Become the world's laziest committer";
+      homepage = "https://github.com/coder/aicommit";
+      license = lib.licenses.cc0;
+      maintainers = with lib.maintainers; [momeemt];
+    };
+  }
