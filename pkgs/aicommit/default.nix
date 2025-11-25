87342aec6bf2532f14f50d89220564ef106787e6
Mutsuha Asada <me@momee.mt>
2025-11-26 03:13:14 +0900

    fix: patch aicommit to support Gemini API

--- pkgs/aicommit/default.nix
+++ pkgs/aicommit/default.nix
@@ -18,6 +18,13 @@ in
 
     vendorHash = "sha256-bZQ0XAbyIr44NZko6O7SkUJBa3rkwblBsSU2jnak/+0=";
 
+    patchPhase = ''
+      runHook prePatch
+      substituteInPlace cmd/aicommit/main.go \
+        --replace-fail 'if resp.Usage != nil {' 'if false && resp.Usage != nil {'
+      runHook postPatch
+    '';
+
     meta = {
       description = "Become the world's laziest committer";
       homepage = "https://github.com/coder/aicommit";
